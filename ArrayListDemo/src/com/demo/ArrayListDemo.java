de18f1927ba408caa8e4b77d68c1972318016a06
tanmayee1312 <tanmayeeburke2020@gmail.com>
2022-10-15 11:20:58 +0530

    ArrayList program is added

---
+++ ArrayListDemo/src/com/demo/ArrayListDemo.java
@@ -0,0 +1,14 @@
+package com.demo;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+	List <String> list = Arrays.asList("tanmayee" , "tejas" , "sampda" , "sujay");
+
+	System.out.println(list);
+	}
+
+}
